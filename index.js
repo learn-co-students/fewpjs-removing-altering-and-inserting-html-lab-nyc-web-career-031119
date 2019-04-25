2311e36e426e86bb45ec147638e3bcf1443f19ba
Yevgeniy <yevgeniyerot@gmail.com>
2019-04-25 14:29:51 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+
+document.querySelector('#main').remove();
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+
+newHeader.innerHTML = 'is the champion';
+
+// document.getElementsByTagName('body')[0].appendChild(newHeader);
