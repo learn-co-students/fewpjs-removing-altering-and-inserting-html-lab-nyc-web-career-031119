9efa1267358a93eb1201176e5341e3e652f6d158
J-Agens <johntagens@gmail.com>
2019-04-25 14:05:09 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+let mainHeader = document.querySelector('main');
+mainHeader.remove();
+
+const newHeader = document.createElement('h1');
+newHeader.id = "victory";
+
+document.body.appendChild(newHeader);
+
+newHeader.innerText = "John is the champion";
